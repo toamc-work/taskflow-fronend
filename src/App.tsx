f6bf3df11a6f5ff09227dfcc5c58e53b40b3a691
toamc-work <toamc.work@gmail.com>
2023-10-25 19:01:53 +0300

    change to hash router becasue github pages is not support in browser history

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { RegisterScreen } from './screens/Register/screens';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 import { useResponsive } from './globalHooks/useResponsive';
 import { LoginScreen } from './screens/Login/screens';
 function App() {
@@ -17,12 +17,12 @@ function App() {
   }, [mobile, tablet, desktop]);
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
           <Route path='/signup' element={<RegisterScreen status={device}/>}/>
           <Route path='/signin' element={<LoginScreen status={device}/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
